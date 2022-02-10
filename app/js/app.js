090038b1c0ecd12bd871854e0cc5aa73c72135d3
Edmond-999 <82581663+Edmond-999@users.noreply.github.com>
2022-02-10 19:33:53 +0300

    add mobile css styles

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,16 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  $(".burger").click(function () {
+    if ($(".nav-ul").hasClass("opened")) {
+      $(".nav-ul").removeClass("opened");
+    } else {
+      $(".nav-ul").addClass("opened");
+    }
+  });
+  $(".burger").click(function () {
+    if ($(".lines").hasClass("opened")) {
+      $(".lines").removeClass("opened");
+    } else {
+      $(".lines").addClass("opened");
+    }
+  });
+});
